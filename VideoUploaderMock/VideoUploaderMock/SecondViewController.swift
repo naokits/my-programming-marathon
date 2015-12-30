234a922842adb9d8808ae22adf8c4a49e7ccb2ba
Naoki Tsutsui <naokitsutsui@me.com>
2015-12-30 22:05:05 +0800

    VideoUploaderMockのプロジェクトを追加

---
+++ VideoUploaderMock/VideoUploaderMock/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  VideoUploaderMock
+//
+//  Created by Naoki Tsutsui on 12/26/15.
+//  Copyright © 2015 Naoki Tsutsui. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
