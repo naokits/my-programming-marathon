8484166fddc5f4e5663fe9e7e1019c04ae6d1517
Naoki Tsutsui <naokitsutsui@me.com>
2016-01-13 22:59:28 +0800

    close #17 接続状態が変わった時にアラート表示するようにした

--- ReachabilityDemo/ReachabilityDemo/ViewController.swift
+++ ReachabilityDemo/ReachabilityDemo/ViewController.swift
@@ -14,6 +14,10 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+//        setupNotification()
+    }
+    
+    override func viewDidAppear(animated: Bool) {
         setupNotification()
     }
 
@@ -52,13 +56,28 @@ class ViewController: UIViewController {
         
         if reachability.isReachable() {
             if reachability.isReachableViaWiFi() {
-                print("Reachable via WiFi")
+                self.showAlertMessage("Reachable via WiFi")
             } else {
-                print("Reachable via Cellular")
+                self.showAlertMessage("Reachable via Cellular")
             }
         } else {
-            print("Not reachable")
+            self.showAlertMessage("Not reachable")
         }
+    }
+    
+    // MARK: - Alert Message
+    
+    func showAlertMessage(message: String!) {
+        let title = "接続状態が変化しました"
+        let alertController = UIAlertController(title: title, message: message, preferredStyle: .Alert)
+        
+        let defaultAction = UIAlertAction(title: "OK", style: .Default, handler: nil)
+        alertController.addAction(defaultAction)
+
+        dispatch_async(dispatch_get_main_queue(), {
+            // UIの操作はメインスレッドで実行する必要がある
+            self.presentViewController(alertController, animated: true, completion: nil)
+        })
     }
 }
 
