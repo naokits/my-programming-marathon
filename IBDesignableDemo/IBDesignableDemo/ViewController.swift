ed9f067f491eb3996902a381f770a4399878880f
Naoki Tsutsui <naokitsutsui@me.com>
2016-01-29 13:15:42 +0800

    IBDesignableを使用して表示するカスタムUIButtonのプロジェクトを追加

---
+++ IBDesignableDemo/IBDesignableDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  IBDesignableDemo
+//
+//  Created by Naoki Tsutsui on 1/29/16.
+//  Copyright © 2016 Naoki Tsutsui. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
