bd985437a1302aef533f4ed65639de18aabb08a8
Naoki Tsutsui <naokitsutsui@me.com>
2016-04-02 22:43:53 +0800

    ライブラリの更新

--- iPhoneSensorDemo/Pods/Target Support Files/Pods-iPhoneSensorDemo/Pods-iPhoneSensorDemo-frameworks.sh
+++ iPhoneSensorDemo/Pods/Target Support Files/Pods-iPhoneSensorDemo/Pods-iPhoneSensorDemo-frameworks.sh
@@ -16,7 +16,7 @@ install_framework()
     local source="$1"
   fi
 
-  local destination="${CONFIGURATION_BUILD_DIR}/${FRAMEWORKS_FOLDER_PATH}"
+  local destination="${TARGET_BUILD_DIR}/${FRAMEWORKS_FOLDER_PATH}"
 
   if [ -L "${source}" ]; then
       echo "Symlinked..."
@@ -84,12 +84,12 @@ strip_invalid_archs() {
 
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
-  install_framework "$CONFIGURATION_BUILD_DIR/RxCocoa/RxCocoa.framework"
-  install_framework "$CONFIGURATION_BUILD_DIR/RxSwift/RxSwift.framework"
-  install_framework "$CONFIGURATION_BUILD_DIR/SwiftyBeaver/SwiftyBeaver.framework"
+  install_framework "$BUILT_PRODUCTS_DIR/RxCocoa/RxCocoa.framework"
+  install_framework "$BUILT_PRODUCTS_DIR/RxSwift/RxSwift.framework"
+  install_framework "$BUILT_PRODUCTS_DIR/SwiftyBeaver/SwiftyBeaver.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
-  install_framework "$CONFIGURATION_BUILD_DIR/RxCocoa/RxCocoa.framework"
-  install_framework "$CONFIGURATION_BUILD_DIR/RxSwift/RxSwift.framework"
-  install_framework "$CONFIGURATION_BUILD_DIR/SwiftyBeaver/SwiftyBeaver.framework"
+  install_framework "$BUILT_PRODUCTS_DIR/RxCocoa/RxCocoa.framework"
+  install_framework "$BUILT_PRODUCTS_DIR/RxSwift/RxSwift.framework"
+  install_framework "$BUILT_PRODUCTS_DIR/SwiftyBeaver/SwiftyBeaver.framework"
 fi
